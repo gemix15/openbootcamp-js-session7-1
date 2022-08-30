3d92ad9b2d77e0d5cfb09a122abc5e7ced4855a4
GemmaBatlle <87717688+gemix15@users.noreply.github.com>
2022-08-30 11:30:15 +0200

    Add files via upload

---
+++ conjuntos.js
@@ -0,0 +1,9 @@
+const nombresFamilia = ["Montse", "Enric", "Jordi", "Eric", "Gemma"];
+const nombresSet = new Set(nombresFamilia);
+console.log(nombresSet);
+
+nombresSet.add("Gemma");
+console.log(nombresSet);
+
+nombresSet.add("Javascript");
+console.log(nombresSet);
